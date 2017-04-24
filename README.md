8f6024f45ab9d54c060c34b180d1614f1be4e746
dandesonjew <danieltanderson@berkeley.edu>
2017-04-24 12:44:03 -0700

    added readme, cleand gameboard to look nice

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TicTacToePython
 A console tic tac toe game created with python
+
+### TODO:
+-Fix print board to display the Game Board with x, y Values
+-Figure out how to find the best possible move for the computer
+    -At clone method to the game board (make it immutable)
+    -Create a min/max tree which stores a game board + current player
+-Clean the game run output, make it friendly to the eyes
+
+
+### Future/Next Steps:
+-Create an HTML representation of the Game Board
+-Create a DIV to handle text output from the python game
+-Style it with CSS
+-Add Django library to deploy and set default route to be the gameboard
+-Figure out how to route the output from the game to the new HTML element
+-Deploy to Heroku and share the Link
--- board.py
+++ board.py
@@ -1,60 +1,79 @@
-class Board:
-  #creates a new empty game board
-  #the board is a 2d array of 0, 1, 2 Empty, Player 1, Player 2
-  def __init__(self):
-    self.game_board = [[0,0,0],[0,0,0],[0,0,0]]
+"""
+Board file, should only be used from the game file,
+and not manipulated directly.
+"""
 
-  #checks to see if this board has a winner
-  def winning_board(self):
-    #check horizontal and vertical
-    for i in range(0,2):
-      if self.game_board[i][0] == self.game_board[i][1] == self.game_board[i][2] != 0:
+class Board(object):
+    """
+    Board Class used to hold a respresentation of the current Game Board
+    By Daniel Anderson
+    """
+    def __init__(self):
+        """#creates a new empty game board
+        #2d array of 0 Empty, 1 Human, 2 Computer
+        """
+        self.game_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+    def winning_board(self):
+        """
+        checks to see if this board has a winner
+        """
+        #horizontal and vertical
+        for i in range(0, 2):
+            if self.game_board[i][0] == self.game_board[i][1] == self.game_board[i][2] != 0:
+                return True
+            if self.game_board[0][i] == self.game_board[1][i] == self.game_board[2][i] != 0:
+                return True
+
+        #diagonals
+        if self.game_board[0][0] == self.game_board[1][1] == self.game_board[2][2] != 0:
+            return True
+        if self.game_board[0][2] == self.game_board[1][1] == self.game_board[2][0] != 0:
+            return True
+
+        return False
+
+    def can_put(self, xcoord, ycoord):
+        """Return true or false if a new piece can be place at that position
+        Arguments:
+            xcoord- row index,
+            ycoord - y index
+        """
+        return self.game_board[xcoord][ycoord] == 0
+
+    def put(self, xcoord, ycoord, player):
+        """Places a new tile on the game board
+        player must be [1, 2]
+        xcoord, ycoord must be [0, 1, 2]
+        """
+        self.game_board[xcoord][ycoord] = player
+
+    def out_of_moves(self):
+        """returns true if there are no more available squares """
+        for i in self.game_board:
+            for j in i:
+                if j == 0:
+                    return False
         return True
-      if self.game_board[0][i] == self.game_board[1][i] == self.game_board[2][i] != 0:
-        return True
 
-    #check diagonals
-    if self.game_board[0][0] == self.game_board[1][1] == self.game_board[2][2] != 0:
-        return True
-    if self.game_board[0][2] == self.game_board[1][1] == self.game_board[2][0] != 0:
-        return True
-    #else return false
-    return False
+    #returns a list of all possible moves
+    def possible_moves(self):
+        """returns a list of all possible moves """
+        move_list = []
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.game_board[i][j] == 0:
+                    move_list.append([i, j])
+        return move_list
 
-  #can we place a new square on the board
-  def can_put(self, x, y):
-    return self.game_board[x][y] == 0
+    def optimal_move(self):
+        """return the best move out of the list of moves"""
+        move_list = self.possible_moves()
+        return move_list[0]
 
-  #player must be 1, or 2
-  #x, y must be within the game board
-  def put(self, x, y, player):
-    self.game_board[x][y] = player
-
-  #returns true if there are no more available squares
-  def out_of_moves(self):
-    for i in self.game_board:
-      for j in i:
-        if j == 0:
-          return False
-    return True
-
-  #returns a list of all possible moves
-  def possible_moves(self):
-    move_list = []
-    for i in range(0,3):
-      for j in range(0,3):
-        if self.game_board[i][j] == 0:
-          move_list.append([i,j])
-    return move_list
-
-
-  #return the best move out of the list of moves
-  def optimal_move(self):
-    move_list = self.possible_moves()
-    return move_list[0]
-
-  def print_board(self):
-    print '----------'
-    for i in self.game_board:
-      print i
-    print "--------------"
+    def print_board(self):
+        """Displays the game board"""
+        print '----------'
+        for i in self.game_board:
+            print i
+        print "--------------"
